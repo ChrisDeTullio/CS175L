e85c91d3581fc7cfc5bcc89d5cf48cd3a98f575a
ChrisDeTullio <146123329+ChrisDeTullio@users.noreply.github.com>
2024-01-18 12:56:09 -0500

    my information v1

---
+++ myInfo.py
@@ -0,0 +1,9 @@
+#CS175L
+#Christopher DeTullio
+#myInfo
+
+print("My name is: Christopher DeTullio")
+print("My address is: 27 Demarest Drive, Manalapan, New Jersey, 07726")
+print("My phone number is: 347-576-5990")
+
+
