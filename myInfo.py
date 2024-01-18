fe0b4c06b4f00365c44ab47a5475cd8b6fc4819a
ChrisDeTullio <146123329+ChrisDeTullio@users.noreply.github.com>
2024-01-18 12:58:38 -0500

    my information v2

--- myInfo.py
+++ myInfo.py
@@ -5,5 +5,4 @@
 print("My name is: Christopher DeTullio")
 print("My address is: 27 Demarest Drive, Manalapan, New Jersey, 07726")
 print("My phone number is: 347-576-5990")
-
-
+print("My major is: Computer Science Bachelor of Arts")
