5ec3a87fbe4c143f202468995284c651ad6a6c62
ChrisDeTullio <146123329+ChrisDeTullio@users.noreply.github.com>
2024-03-27 15:17:42 -0400

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -1,13 +1,33 @@
 #Christopher DeTullio
 #CS175L
 #AverageFromInput
+
 def main():
-    numbers = []
-    with open("/Users/chrisdetullio314/Downloads/numbers.txt") as file:
-        for i, line in enumerate(file, 1):
-            numbers.append(float(line.strip()))
-            print("I read in", i, "number(s). Current number is:", line.strip())
-    average = sum(numbers) / len(numbers)
-    print("Average:", average)
+    total = 0
+    try:
+        numbers = []
+        line_number = 1
+        with open("/Users/chrisdetullio314/Downloads/numbers.txt") as file:
+            for i, line in enumerate(file, 1):
+                try:
+                    number = float(line.strip())
+                    numbers.append(number)
+                    total += number
+                    print(f"I read in {line_number} number(s). Current number is: {number:.2f}. Total is: {total:.2f}")
+                    line_number += 1
+                    
+                except ValueError as e:
+                    print(f"Bad data: {line.strip()} skipping")
+
+        if numbers:
+            average = sum(numbers) / len(numbers)
+            print("Average:", average)
+            
+        else:
+            print("There are no numbers in this file.")
+
+    except IOError as e:
+        print(f"SystemExit: File not found: '{e.filename}' - exiting")
+   
 if __name__ == '__main__':
     main()
